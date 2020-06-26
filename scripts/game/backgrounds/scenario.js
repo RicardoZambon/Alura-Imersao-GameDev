05a075a8dadd2825f503728886a3f51f0a4c7fb0
Ricardo Zambon <ricardo.zambon@smartm.com>
2020-06-25 23:55:04 -0300

    added parallax effect.

--- scripts/game/backgrounds/scenario.js
+++ scripts/game/backgrounds/scenario.js
@@ -1,44 +1,52 @@
-const background1_file = 'images/scenario/florest1.png';
-const background2_file = 'images/scenario/florest2.png';
+const background_files = [
+    ['images/scenario/florest1/Sky.png', 'images/scenario/florest1/BG_Decor.png', 'images/scenario/florest2/Middle_Decor.png', 'images/scenario/florest1/ForeGround.png', 'images/scenario/florest1/Ground.png'],
+    ['images/scenario/florest2/Sky.png', 'images/scenario/florest2/BG_Decor.png', 'images/scenario/florest2/Middle_Decor.png', 'images/scenario/florest2/ForeGround.png', 'images/scenario/florest2/Ground.png'],
+    ['images/scenario/florest3/Sky.png', 'images/scenario/florest3/BG_Decor.png', 'images/scenario/florest3/Middle_Decor.png', 'images/scenario/florest3/ForeGround.png', 'images/scenario/florest3/Ground.png'],
+    ['images/scenario/florest4/Sky.png', 'images/scenario/florest4/BG_Decor.png', 'images/scenario/florest4/Middle_Decor.png', 'images/scenario/florest4/ForeGround.png', 'images/scenario/florest4/Ground.png']
+];
 
 class ScenarioBackground {
     constructor() {
-        this.background1_Image = null;
-        this.background1_Speed = 4;
-        this.background1_X1 = 0;
-        
-        this.background2_Image = null;
-        this.background2_Speed = 2;
-        this.background2_X1 = 0;
+        this.background = [];
+
+        this.backgroundSpeeds = [1, 2, 4, 6, 8];
+        this.backgroundX1 = [0, 0, 0, 0, 0];
+
+        //TODO: change background after some levels.
+        this.currentBackground = 0;
     }
 
     preload() {
-        this.background1_Image = loadImage(background1_file);
-        this.background2_Image = loadImage(background2_file);
+        for (var b = 0; b < background_files.length; b++) {
+            var background = [];
+            for (var i = 0; i < background_files[b].length; i++) {
+                background.push(loadImage(background_files[b][i]));
+            }
+            this.background.push(background);
+        }
     }
 
     show() {
-        image(this.background2_Image, this.background2_X1, 0, width, height);
-        image(this.background2_Image, width + this.background2_X1, 0, width, height);
+        var background = this.background[this.currentBackground];
+        for (var i = 0; i < background.length; i++) {
+            var backgroundImage = background[i];
 
-        image(this.background1_Image, this.background1_X1, 0, width, height);
-        image(this.background1_Image, width + this.background1_X1, 0, width, height);
-
+            image(backgroundImage, this.backgroundX1[i], 0, width, height);
+            image(backgroundImage, width + this.backgroundX1[i], 0, width, height);
+        }
         this.move();
     }
 
     move() {
-        this.background1_X1 = this.background1_X1 - this.background1_Speed;        
+        var background = this.background[this.currentBackground];
+        for (var i = 0; i < background.length; i++) {
+            var image = background[i];
 
-        if (this.background1_X1 < -width) {
-            this.background1_X1 = 0;
-        }
+            this.backgroundX1[i] -= this.backgroundSpeeds[i];
 
-
-        this.background2_X1 = this.background2_X1 - this.background2_Speed;
-
-        if (this.background2_X1 < -width) {
-            this.background2_X1 = 0;
+            if (this.backgroundX1[i] < -width) {
+                this.backgroundX1[i] = 0;
+            }
         }
     }
 }
